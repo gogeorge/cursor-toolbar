1edf9f25cbd53f38ee453dd04b3a6d08afbddd47
gogeorge <grvaltas@gmail.com>
2026-04-04 04:17:36 +0300

    Initial Commit

---
+++ cursor-toolbar/cursor_toolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursor_toolbarApp.swift
+//  cursor-toolbar
+//
+//  Created by George Valtas on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct cursor_toolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cursor-toolbarTests/cursor_toolbarTests.swift
@@ -0,0 +1,19 @@
+//
+//  cursor_toolbarTests.swift
+//  cursor-toolbarTests
+//
+//  Created by George Valtas on 04/04/2026.
+//
+
+import Testing
+@testable import cursor_toolbar
+
+struct cursor_toolbarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
